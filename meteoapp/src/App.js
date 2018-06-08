dbf9b44f229ee85d475ccd456968decc256373dc
frederic <fredydope@gmail.com>
2018-06-08 15:03:29 +0200

    recuperation de l'API avec fetch et form input plus button

--- meteoapp/src/App.js
+++ meteoapp/src/App.js
@@ -4,7 +4,16 @@ import Title from './components/Title'
 import Form from './components/Form'
 import Weather from './components/Weather'
 
+const API_KEY = 'd962b7ceebc42bf6f5b5a65db30521c3'
+
 class App extends React.Component {
+//creation d'une fonction qui get l'API
+  getWeather = async () =>{
+  const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&units=metric&appid=${API_KEY}`)
+// api call devient json = data  
+  const data = await api_call.json()
+  }
+
   render() {
     return (
       <div className="App">
--- meteoapp/src/components/Form.js
+++ meteoapp/src/components/Form.js
@@ -3,11 +3,14 @@ import React from 'react';
 class Form extends React.Component {
   render() {
     return (
-      <div>
-      <h2>Form Component</h2>
+      <form>
+
+      <input type='text' name='city' placeholder='city...'/>
+      <input type='text' name='country' placeholder='country...'/>
+      <button>Get Weather</button>
         
-      </div>
-    );
+      </form>
+    )
   }
 }
 
