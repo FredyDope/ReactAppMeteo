56e29c9f58c07926f4001e8816cc24003a5fec6b
frederic <fredydope@gmail.com>
2018-06-08 15:39:45 +0200

    ajout des props/preventdefault

--- meteoapp/src/App.js
+++ meteoapp/src/App.js
@@ -8,17 +8,20 @@ const API_KEY = 'd962b7ceebc42bf6f5b5a65db30521c3'
 
 class App extends React.Component {
 //creation d'une fonction qui get l'API
-  getWeather = async () =>{
-  const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&units=metric&appid=${API_KEY}`)
+  getWeather = async (e) =>{
+  e.preventDefault()//empeche la page de se mettre à jour automatiquementpour afficher les donnés
+  const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}&units=metric`)
 // api call devient json = data  
   const data = await api_call.json()
-  }
 
+  console.log(data)
+  }
+//dans Form nous definissons une props getWeather on envoi sa valeur dans la fonction getWeather qui retourn dans Form.JS
   render() {
     return (
       <div className="App">
       <Title />
-      <Form />
+      <Form getWeather={this.getWeather}/>
       <Weather />      
       </div>
     );
--- meteoapp/src/components/Form.js
+++ meteoapp/src/components/Form.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 class Form extends React.Component {
   render() {
+  	//react attribut onsubmit appel this.props.getweather dans app.js
     return (
-      <form>
+      <form onSubmit ={this.props.getWeather}>
 
       <input type='text' name='city' placeholder='city...'/>
       <input type='text' name='country' placeholder='country...'/>
