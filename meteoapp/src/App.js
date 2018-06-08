77a0d9e008b361b71a7f3803151d1b7ff7f9df78
frederic <fredydope@gmail.com>
2018-06-08 14:03:23 +0200

    creation dossier components et de 3 components

--- meteoapp/src/App.js
+++ meteoapp/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import Title from './components/Title'
+import Form from './components/Form'
+import Weather from './components/Weather'
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-      <h2>test ok!</h2>
-        
+      <Title />
+      <Form />
+      <Weather />      
       </div>
     );
   }
---
+++ meteoapp/src/components/Title.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Title extends React.Component {
+  render() {
+    return (
+      <div>
+      <h2>Weather Finder</h2>
+
+      <p>Find out Temperatures...</p>
+
+        
+      </div>
+    );
+  }
+}
+
+export default Title;
